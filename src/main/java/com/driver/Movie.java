c50e2523a8914698b14e5902101acc915ba0e37d
Varis1807 <vrvarisrana123@gmail.com>
2022-12-02 10:09:41 +0530

    initial Commit

--- src/main/java/com/driver/Movie.java
+++ src/main/java/com/driver/Movie.java
@@ -1,4 +1,49 @@
 package com.driver;
 
 public class Movie {
+    private int id;
+    private String name;
+    private int durationInMinutes;
+    private double imdbRating;
+    public Movie(){
+
+    }
+    public Movie(int id,String name, int durationInMinutes, double imdbRating){
+        this.id=id;
+        this.name=name;
+        this.durationInMinutes=durationInMinutes;
+        this.imdbRating=imdbRating;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDurationInMinutes() {
+        return durationInMinutes;
+    }
+
+    public void setDurationInMinutes(int durationInMinutes) {
+        this.durationInMinutes = durationInMinutes;
+    }
+
+    public double getImdbRating() {
+        return imdbRating;
+    }
+
+    public void setImdbRating(double imdbRating) {
+        this.imdbRating = imdbRating;
+    }
 }
--- src/main/java/com/driver/MovieController.java
+++ src/main/java/com/driver/MovieController.java
@@ -1,4 +1,24 @@
 package com.driver;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MovieController {
+
+    //Creating database
+    Map<Integer, Movie> moviesDb = new HashMap<>();
+
+    //Creating Api's
+//    1--> Add a movie: POST /movies/add-movie
+//    Pass the Movie object as request body
+//    Return success message wrapped in a ResponseEntity object
+//    Controller Name - addMovie
+    @PostMapping("/movies/add-movie")
+    public void add_Movie(@RequestBody(required = true) Movie m){
+        moviesDb.put(m.getId(),m);
+        System.out.println();
+    }
 }
